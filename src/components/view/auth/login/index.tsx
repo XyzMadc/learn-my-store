00177760a17dc74d76439ee85c4c3d5f8576a899
synx <ahmadruddin17@gmail.com>
2024-08-08 14:55:06 +0700

    fix: rename login input

--- src/components/view/auth/login/index.tsx
+++ src/components/view/auth/login/index.tsx
@@ -8,15 +8,15 @@ export default function LoginView() {
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
+              htmlFor="email"
             >
-              Username
+              Email
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
+              id="email"
+              type="email"
+              placeholder="Masukan Email Anda"
             />
           </div>
           <div className="mb-6">
@@ -30,11 +30,8 @@ export default function LoginView() {
               className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder="******************"
+              placeholder="********"
             />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
           </div>
           <div className="flex items-center justify-between">
             <button
